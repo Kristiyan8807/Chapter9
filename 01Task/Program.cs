af43fbe6dc458789a2c449d1ca711ce691b88f71
kristiyan8807 <kristiyan8807@gmail.com>
2019-10-22 23:15:08 +0300

    Chapter9

---
+++ 01Task/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишете метод, който при подадено име отпечатва на конзолата
+//"Hello, <name>!" (например "Hello, Peter!"). Напишете програма,
+//която тества дали този метод работи правилно.
+using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Напишете си името: ");
+            string name = Console.ReadLine();
+            UserName(name);
+        }
+        static void UserName(string name)
+        {
+            Console.WriteLine($"Здравей {name}");
+        }
+    }
---
+++ 02Task/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Напишете първото число: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Напишете второто число: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.Write("Напишете третото число: ");
+        int num3 = int.Parse(Console.ReadLine());
+        GetMax(num1, num2, num3);
+    }
+
+    static void GetMax(int num1, int num2, int num3)
+    {
+        if (num1 > num2)
+        {
+            if (num1 > num3)
+            {
+                Console.Write("Първото число е най-голямо: {0}", num1);
+            }
+            else
+            {
+                Console.Write("Третото число е най-голямо: {0}", num3);
+            }
+        }
+        else if (num2 > num3)
+            Console.Write("Второто число е най-голямо: {0}", num2);
+        else
+            Console.Write("Третото число е най-голямо: {0}", num3);
+    }
+}
