d4f461dd96eaf777d7999458c6174162b3d7fca4
kristiyan8807 <kristiyan8807@gmail.com>
2019-10-24 23:21:13 +0300

    Chapter9

---
+++ 04Task/Program.cs
@@ -0,0 +1,33 @@
+﻿//Напишете метод, който намира колко пъти дадено число се среща
+//в даден масив.Напишете програма, която проверява дали този метод
+//работи правилно.
+using System;
+using System.Linq;
+
+class Program
+{
+    public static void Main()
+    {
+        int[] array = { 12, 5, 12, 2, 3, 3, 4, 4, 4, 6, 7, 7, 9, 11, 22, 22 };
+        CheckingNumberInArray(array);
+    }
+    static void CheckingNumberInArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            int count = 0;
+            for (int j = 0; j < array.Length; j++)
+            {
+
+                if (array[i] == array[j])
+                    count = count + 1;
+            }
+            Console.WriteLine(" " + array[i] + " is repeated " + count + " times");
+        }
+    }
+}
+
+
+
+
+
---
+++ 05Task/Program.cs
@@ -0,0 +1,62 @@
+﻿//Напишете метод, който проверява дали елемент, намиращ се на дадена
+//позиция от масив, е по-голям от двата му съседа.Тествайте метода
+//дали работи коректно.
+using System;
+
+namespace _05Task
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter array length: ");
+            int length = Int32.Parse(Console.ReadLine());
+
+            int[] arr = new int[length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write("Enter {0} element: ", i);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.Write("Enter position in array: ");
+            int position = Int32.Parse(Console.ReadLine());
+
+            CompareNumbers(arr, position);
+        }
+
+        static void CompareNumbers(int[] array, int pos)
+        {
+            if (array[pos] > array[pos - 1] && array[pos] > array[pos + 1])
+            {
+                Console.WriteLine("{0} is bigger than left and right neighbours", array[pos]);
+            }
+            if (array[pos] < array[pos - 1] && array[pos] < array[pos + 1])
+            {
+                Console.WriteLine("{0} is smaller than left and right neighbours", array[pos]);
+            }
+            if (array[pos] < array[pos - 1])
+            {
+                Console.WriteLine("{0} is smaller than left neighbours", array[pos]);
+            }
+            if (array[pos] < array[pos + 1])
+            {
+                Console.WriteLine("{0} is smaller than right neighbours", array[pos]);
+            }
+            if (array[pos] == array[pos + 1])
+            {
+                Console.WriteLine("{0} is equal than right neighbours", array[pos]);
+            }
+            if (array[pos] == array[pos + -1])
+            {
+                Console.WriteLine("{0} is equal than left neighbours", array[pos]);
+            }
+            if (array[pos] == array[pos + -1] && array[pos] == array[pos + +1])
+            {
+                Console.WriteLine("{0} is equal than left neighbours and right neighbours", array[pos]);
+            }
+        }
+    }
+}
+
